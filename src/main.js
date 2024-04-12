0af9d79742a7851490dbbd933e85204d29528722
Sirojiddin Aripov <aripov86cm@gmail.com>
2024-04-12 19:31:15 -0400

    All the clock pattern executions are in place.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,49 @@
-let repeat;
-let duration;
+//Either constant or varied changes based on radio button input.
+mode = 'varied';
 
+//text fields so we don't have to grab them each time
+const repeatText = document.getElementById('repetitions')
+const durationText = document.getElementById('duration')
+const patternText = document.getElementById('pattern')
+const paragraph = document.getElementById('test')
+
+//process used input and call according function to execute pattern.
 const setValues = () => {
-  repeat = document.getElementById('repetitions').value;
-  duration = document.getElementById('duration').value;
-  alert(`${repeat} and ${duration}`);
-};
-
-document.getElementById('submit').addEventListener("click", () => {
-  setValues();
-});
-
-document.getElementById('start').addEventListener("click", () => {
-  for (let i = 0; i < repeat; i += 1) {
-    window.vibrate(duration);
+  if (mode === 'constant') {
+    repeat = repeatText.value;
+    duration = durationText.value;
+    executeConstantPattern(repeat, duration);
+  } else if (mode === 'varied') {
+    pattern = patternText.value.split(', ')
+    executeVariedPattern(pattern)
   }
-});
+};
+runClock = true
+const delay = (ms) => {
+  if (!runClock) { return }
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+const clearTimer = (timer) => { clearInterval(timer) }
+
+const executeConstantPattern = async (repeat, duration) => {
+  paragraph.innerHTML = '';
+  for (let i = 0; i < repeat; i += 1) {
+    paragraph.innerHTML += `${duration} `;
+    await delay(duration);
+  }
+}
+
+const executeVariedPattern = async (pattern) => {
+  paragraph.innerHTML = ''
+  for (const duration of pattern) {
+    paragraph.innerHTML += `${duration} `
+    await delay(duration);
+  };
+}
+
+
+document.getElementById('start').addEventListener('click', () => {
+  runClock = true
+  setValues()
+})
+document.getElementById('stop').addEventListener('click', () => { runClock = false })
